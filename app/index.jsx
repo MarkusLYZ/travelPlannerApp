bf786bea8e61f7c530bcea5992dbc171079d95e5
MarkusLYZ <markoos203@gmail.com>
2024-10-25 22:24:51 +0800

    loginPageUpdate

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,67 @@
-import { Image, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import Login from "./../components/Login";
+import { Colors } from "@/constants/Colors";
 
 export default function Index() {
   return (
     <View>
       <Image
-        style={{ width: "100%", height: 500 }}
+        style={{ width: "100%", height: 400 }}
         source={require("./../assets/login.jpg")}
       />
-      <View>
-        <Text style={{fontSize:28, fontFamily:'outfit-bold'}}>AI Travel Planner</Text>
+      <View style={styles.container}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontFamily: "outfit-bold",
+            textAlign: "center",
+            marginTop:10
+          }}
+        >
+          AI Travel Planner
+        </Text>
+        <Text
+          style={{
+            fontFamily: "outfit",
+            fontSize: 17,
+            textAlign: "center",
+            color: Colors.GRAY,
+            marginTop:20
+          }}
+        >
+          Discover your next adventure effortlessly. Personalized itineraries at
+          your fingertips. Travel smarter with AI driven insights.
+        </Text>
+        <View style={styles.button}>
+          <Text
+            style={{
+              color: Colors.WHITE,
+              textAlign: "center",
+              fontFamily: "outfit",
+              fontSize: 17,
+            }}
+          >
+            Sign In With Google
+          </Text>
+        </View>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    marginTop: -20,
+    height: "100%",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 25,
+  },
+  button: {
+    padding: 15,
+    backgroundColor: Colors.PRIMARY,
+    borderRadius:99,
+    marginTop: '25%'
+  },
+});
