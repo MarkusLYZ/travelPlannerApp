b9dcdd784fa46480923f1e078cab21780ce091a3
MarkusLYZ <markoos203@gmail.com>
2024-10-26 20:54:54 +0800

    LoginPageUpdate

--- app/index.jsx
+++ app/index.jsx
@@ -1,66 +1,10 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import { Colors } from "@/constants/Colors";
+import { Image,  Text, View } from "react-native";
+import Login from "./../components/Login";
 
 export default function Index() {
   return (
     <View>
-      <Image
-        style={{ width: "100%", height: 440 }}
-        source={require("./../assets/images/login.jpg")}
-      />
-      <View style={styles.container}>
-        <Text
-          style={{
-            fontSize: 30,
-            fontFamily: "outfit-bold",
-            textAlign: "center",
-            marginTop:10
-          }}
-        >
-          AI Travel Planner
-        </Text>
-        <Text
-          style={{
-            fontFamily: "outfit",
-            fontSize: 17,
-            textAlign: "center",
-            color: Colors.GRAY,
-            marginTop:20
-          }}
-        >
-          Discover your next adventure effortlessly. Personalized itineraries at
-          your fingertips. Travel smarter with AI driven insights.
-        </Text>
-        <View style={styles.button}>
-          <Text
-            style={{
-              color: Colors.WHITE,
-              textAlign: "center",
-              fontFamily: "outfit",
-              fontSize: 17,
-            }}
-          >
-            Sign In With Google
-          </Text>
-        </View>
-      </View>
+      <Login />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: Colors.WHITE,
-    marginTop: -20,
-    height: "100%",
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    padding: 25,
-  },
-  button: {
-    padding: 15,
-    backgroundColor: Colors.PRIMARY,
-    borderRadius:99,
-    marginTop: '25%'
-  },
-});
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,10 +1,66 @@
-import { View, Text } from 'react-native'
+import { Image, View, StyleSheet,Text } from 'react-native'
 import React from 'react'
+import { Colors } from "@/constants/Colors";
 
 export default function login() {
   return (
     <View>
-      <Text>login</Text>
+      <Image
+        style={{ width: "100%", height: 440 }}
+        source={require("./../assets/images/login.jpg")}
+      />
+      <View style={styles.container}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontFamily: "outfit-bold",
+            textAlign: "center",
+            marginTop:10
+          }}
+        >
+          AI Travel Planner
+        </Text>
+        <Text
+          style={{
+            fontFamily: "outfit",
+            fontSize: 17,
+            textAlign: "center",
+            color: Colors.GRAY,
+            marginTop:20
+          }}
+        >
+          Discover your next adventure effortlessly. Personalized itineraries at
+          your fingertips. Travel smarter with AI driven insights.
+        </Text>
+        <View style={styles.button}>
+          <Text
+            style={{
+              color: Colors.WHITE,
+              textAlign: "center",
+              fontFamily: "outfit",
+              fontSize: 17,
+            }}
+          >
+            Sign In With Google
+          </Text>
+        </View>
+      </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    marginTop: -20,
+    height: "100%",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 25,
+  },
+  button: {
+    padding: 15,
+    backgroundColor: Colors.PRIMARY,
+    borderRadius:99,
+    marginTop: '25%'
+  },
+});
