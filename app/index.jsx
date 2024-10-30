a5868d67550237a1f6a20f28c31414200e4c2d45
MarkusLYZ <markoos203@gmail.com>
2024-10-30 15:38:11 +0800

    sessionAuthUpdate

--- app/index.jsx
+++ app/index.jsx
@@ -2,8 +2,20 @@ import { Image, Text, View } from "react-native";
 import { auth } from "./../configs/FirebaseConfig";
 import Login from "./../components/Login";
 import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function Index() {
-  const user = auth.currentUser;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    // Unsubscribe from the listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
+
   return <View>{user ? <Redirect href={"/mytrip"} /> : <Login />}</View>;
 }
