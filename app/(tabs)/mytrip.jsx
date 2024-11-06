f91a76ed2152d88b88648ff04ab1abd56d7aa153
MarkusLYZ <markoos203@gmail.com>
2024-11-06 23:15:06 +0800

    homePageUpdate

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { Colors } from "../../constants/Colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function MyTrip() {
   return (
-    <View>
-      <Text>mytrip</Text>
+    <View
+      style={{
+        padding: 25,
+        paddingTop: 50,
+        backgroundColor: Colors.WHITE,
+        height: "100%",
+      }}
+    >
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignContent: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text style={{ fontFamily: "outfit-bold", fontSize: 35 }}>My Trip</Text>
+        <Ionicons name="add-circle" size={50} color="black" />
+      </View>
     </View>
-  )
+  );
 }
