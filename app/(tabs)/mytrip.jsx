665a0d35c440db5433b154f47323cc892649a8f6
MarkusLYZ <markoos203@gmail.com>
2024-11-07 15:05:41 +0800

    mytipUpdate

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Colors } from "../../constants/Colors";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import StartNewTripCard from "../../components/MyTrips/StartNewTripCard";
 
 export default function MyTrip() {
+  const [userTrips, setUserTrips] = useState([]);
   return (
     <View
       style={{
@@ -24,6 +26,7 @@ export default function MyTrip() {
         <Text style={{ fontFamily: "outfit-bold", fontSize: 35 }}>My Trip</Text>
         <Ionicons name="add-circle" size={50} color="black" />
       </View>
+      {userTrips?.length==0?<StartNewTripCard/>:null}
     </View>
   );
 }
